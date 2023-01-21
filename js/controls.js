aa3b3108f937ccd3bbe77066dcc3f6ddd125fb8c
Jonathan <jonathanneves33@gmail.com>
2023-01-20 21:40:25 -0300

    initial commit

---
+++ js/controls.js
@@ -0,0 +1,8 @@
+function resetControls() {
+  buttonStop.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+}
+
+export default resetControls
